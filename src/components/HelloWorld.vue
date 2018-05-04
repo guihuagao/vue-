c0d5a6cfe862ff40a23e248cd4219095a6744473
melody <2475352924@qq.com>
2018-05-04 11:21:16 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -21,6 +21,32 @@ export default {
     return {
 
     }
+  },
+  methods:{
+      getContent(id){
+      this.$http.HttpGet({url:"/api/cross/getMenuById"},{id:id})
+        .then((data) => {
+          this.position=data.data[0].menu.fname+' >> '+data.data[0].menu.v_name;
+          this.content=data.data[0].menu.content;
+        }).catch((err)=>{
+
+      })
+    },
+
+
+    toSave(menu) {
+      this.$http.HttpPost({url: "/api/auth/addMenu"}, {fid: menu.id, name: this.menuName1})
+        .then((data) => {
+          this.dialogVisible = false;
+          this.menuName1 = '';
+          this.$message({
+            message: '添加成功！',
+            type: 'success'
+          });
+        }).catch((err) => {
+
+      })
+    }
   }
 }
 </script>
